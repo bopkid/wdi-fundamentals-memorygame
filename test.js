4aed76035f1d73f6d8337dff858042711c519d20
alvin wong <alwong125@gmail.com>
2020-03-04 21:24:35 -0800

    create game logic

---
+++ test.js
@@ -0,0 +1,12 @@
+// this is a comment // gCfs
+let myNumber = 30;
+
+console.log(myNumber);
+myNumber = 20;
+
+myNumber+= 5;
+const  greeting = "Hello";
+const name = "Margaret";
+let sayHello = greeting+' '+ name;
+console.log(myNumber);
+console.log(sayHello);
