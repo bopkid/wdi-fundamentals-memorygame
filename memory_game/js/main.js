094564962685df881eadbcb7d195098d423a0902
alvin wong <alwong125@gmail.com>
2020-03-05 16:21:40 -0800

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,25 @@
-let cardOne = "queen";
-let cardTwo = "queen";
+
+let card = ["queen","queen","king","king"];
+let cardOne = card[0];
+let cardTwo = card[2];
 let cardThree = "king";
 let cardFour = "king";
 
-console.log("user flipper " + cardOne);
+let cardsInPlay = [];
+
+
+
+cardsInPlay.push(cardOne);
+console.log('User flipped '+cardsOne)
+cardsInPlay.push(cardTwo);
+console.log('User flipped '+cardsTwo)
+
+
+if(cardsInPlay.length === 2){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    }
+    else{
+        alert('Sorry, try again');
+    }
+}
--- test.js
+++ test.js
@@ -1,12 +1,22 @@
-// this is a comment // gCfs
-let myNumber = 30;
+const favoriteMovie = "Star Wars";
+let moviePhrase;
+switch(favoriteMovie){
+    case("jaws"):
+        moviePhrase = "You're gonna need a bigger boat";
+        break;
+        case("the shining"):
+        moviePhrase = "All work and no play makes Jack a dull boy";
+        break;
+        case("star wars"):
+        moviePhrase = "Do. Or do not. There is no try.";
+        break;
+        case("forrest gump"):
+        moviePhrase = "Life was like a box of chocolates";
+        break;
+        case("back to the future"):
+        moviePhrase = "Where we're going, we don't need roads.";
+        break;
+        default:
+            moviePhrase = 'Great movie choice!';
 
-console.log(myNumber);
-myNumber = 20;
-
-myNumber+= 5;
-const  greeting = "Hello";
-const name = "Margaret";
-let sayHello = greeting+' '+ name;
-console.log(myNumber);
-console.log(sayHello);
+}
