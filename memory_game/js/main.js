5caf04715beaf8648cbee05d6c27ed243110acec
alvin wong <alwong125@gmail.com>
2020-03-06 15:39:32 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,33 @@
 
-let card = ["queen","queen","king","king"];
-let cardOne = card[0];
-let cardTwo = card[2];
-let cardThree = "king";
-let cardFour = "king";
+let cards = ["queen","queen","king","king"];
 
 let cardsInPlay = [];
 
-
-
-cardsInPlay.push(cardOne);
-console.log('User flipped '+cardsOne)
-cardsInPlay.push(cardTwo);
-console.log('User flipped '+cardsTwo)
-
-
-if(cardsInPlay.length === 2){
-    if(cardsInPlay[0] === cardsInPlay[1]){
-        alert("You found a match!");
-    }
-    else{
-        alert('Sorry, try again');
+function checkForMatch(){
+        
+    if(cardsInPlay.length === 2){
+        if(cardsInPlay[0] === cardsInPlay[1]){
+            alert("You found a match!");
+        }
+        else{
+            alert('Sorry, try again');
+        }
     }
 }
+
+
+function flipCard(cardId){
+
+    let cardOne = cards[0];
+    let cardTwo = cards[2];
+    let cardThree = cards[1];
+    let cardFour = cards[3];
+
+    
+    console.log('User flipped ' +cards[cardId]);
+    
+    cardsInPlay.push(cards[cardId]);
+
+    checkForMatch();
+}
+
